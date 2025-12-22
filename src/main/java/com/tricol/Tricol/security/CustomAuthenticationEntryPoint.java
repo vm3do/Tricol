9908588508afcd7239720e0b3772e33062dc7ebf
vm3do <medoavvadi@gmail.com>
2025-12-22 14:33:47 +0100

    feat: add custom AuthenticationEntryPoint & AccessDeniedHandler

---
+++ src/main/java/com/tricol/Tricol/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.tricol.Tricol.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tricol.Tricol.dto.response.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ErrorResponse errorResp = ErrorResponse.builder()
+                .status(HttpServletResponse.SC_UNAUTHORIZED)
+                .error("Unauthorized")
+                .message(authException.getMessage())
+                .build();
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResp));
+    }
+}
