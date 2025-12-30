fbab4ed4ca9129ea41e878b48ebc6f3fa8cc9192
vm3do <medoavvadi@gmail.com>
2025-12-30 16:27:18 +0100

    fix: update userDetailsService injection from setter to constructor in the SecurityConfig

--- src/main/java/com/tricol/Tricol/config/SecurityConfig.java
+++ src/main/java/com/tricol/Tricol/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
